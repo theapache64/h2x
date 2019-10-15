745253489452af4e8ceb9a1a2ceecd8f81e1caae
theapache64 <theapache64@gmail.com>
2019-10-15 16:14:47 +0530

    🌟 add payment callback dialogs

--- android/app/src/main/java/com/theapache64/h2x/App.kt
+++ android/app/src/main/java/com/theapache64/h2x/App.kt
@@ -4,11 +4,11 @@ import android.app.Activity
 import android.app.Application
 import com.theapache64.h2x.data.repositories.UserPrefRepository
 import com.theapache64.h2x.di.components.DaggerAppComponent
+import com.theapache64.openupi.OpenUPI
 import com.theapache64.twinkill.TwinKill
 import com.theapache64.twinkill.di.modules.ContextModule
 import com.theapache64.twinkill.googlefonts.GoogleFonts
 import com.theapache64.twinkill.network.di.modules.BaseNetworkModule
-import com.theapache64.twinkill.network.utils.retrofit.interceptors.AuthorizationInterceptor
 import com.theapache64.twinkill.network.utils.retrofit.interceptors.CurlInterceptor
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -45,6 +45,11 @@ class App : Application(), HasActivityInjector {
                 .addOkHttpInterceptor(CurlInterceptor())
                 .setDefaultFont(GoogleFonts.GoogleSansRegular)
                 .build()
+        )
+
+        OpenUPI.init(
+            "8301893919@ybl",
+            "Shifar"
         )
 
     }
--- android/app/src/main/java/com/theapache64/h2x/ui/activities/payment/PaymentActivity.kt
+++ android/app/src/main/java/com/theapache64/h2x/ui/activities/payment/PaymentActivity.kt
@@ -1,15 +1,20 @@
 package com.theapache64.h2x.ui.activities.payment
 
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.theapache64.h2x.R
 import com.theapache64.h2x.databinding.ActivityPaymentBinding
 import com.theapache64.h2x.models.FormItem
+import com.theapache64.openupi.OpenUPI
+import com.theapache64.openupi.TransactionCallback
+import com.theapache64.openupi.TransactionResult
 import com.theapache64.twinkill.network.utils.Resource
 import com.theapache64.twinkill.ui.activities.base.BaseAppCompatActivity
 import com.theapache64.twinkill.utils.extensions.bindContentView
@@ -35,6 +40,7 @@ class PaymentActivity : BaseAppCompatActivity(), PaymentHandler {
     }
 
 
+    private lateinit var formItems: java.util.ArrayList<FormItem>
     @Inject
     lateinit var factory: ViewModelProvider.Factory
 
@@ -53,7 +59,7 @@ class PaymentActivity : BaseAppCompatActivity(), PaymentHandler {
         binding.handler = this
         binding.viewModel = viewModel
 
-        val formItems = intent.getSerializableExtra(KEY_FORM_ITEMS) as ArrayList<FormItem>
+        this.formItems = intent.getSerializableExtra(KEY_FORM_ITEMS) as ArrayList<FormItem>
 
         viewModel.getPrefs().observe(this, Observer {
             val iContentPayment = binding.iContentPayment
@@ -82,8 +88,70 @@ class PaymentActivity : BaseAppCompatActivity(), PaymentHandler {
     }
 
     override fun onPayClicked() {
-
+        OpenUPI.newTransaction(viewModel.totalPayable.get())
+            .start(this)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        OpenUPI.handleActivityResult(
+            viewModel.totalPayable.get(),
+            requestCode,
+            resultCode,
+            data,
+            object : TransactionCallback {
+                override fun onFailure(message: String, transactionResult: TransactionResult?) {
+                    showAlertDialog(
+                        R.string.title_payment_failed,
+                        message
+                    )
+                }
+
+                override fun onSubmitted(transactionResult: TransactionResult) {
+                    showAlertDialog(
+                        R.string.title_payment_submitted,
+                        R.string.message_payment_submitted
+                    ) {
+
+                    }
+                }
+
+                override fun onSuccess(transactionResult: TransactionResult) {
+                    showAlertDialog(
+                        R.string.title_payment_success,
+                        R.string.message_payment_success
+                    ) {
+                        finish()
+                    }
+                }
+
+            })
+    }
+
+    private fun showAlertDialog(title: Int, message: Any, onOk: () -> Unit) {
+        val alert = AlertDialog.Builder(this@PaymentActivity)
+            .setTitle(title)
+            .setCancelable(false)
+            .setPositiveButton(android.R.string.ok) { dialogInterface: DialogInterface, i: Int ->
+                onOk()
+            }
+
+        when (message) {
+            is String -> {
+                alert.setMessage(message)
+            }
+            is Int -> {
+                alert.setMessage(message)
+            }
+            else -> throw IllegalArgumentException("Alert message should be either String or Int")
+        }
+
+
+        alert.create().show()
+    }
+
+
 }
 
 
